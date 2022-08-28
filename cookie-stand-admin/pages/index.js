1164f67edff758f796f8d5215247cda7f8749752
essa eayd abu kheir <esaaeyad22@gmail.com>
2022-08-27 20:28:25 -0700

    cookie-stand-admin

--- cookie-stand-admin/pages/index.js
+++ cookie-stand-admin/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Footer from '../components/Footer'
+import Header  from './Header'
+import Main from './Main'
+import Footer from './Footer'
 
 
 export default function Home() {
